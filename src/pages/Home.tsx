a0d48e974c55f928e88fd0e78c8733d60d89bcbd
himdol <himdol8833@gmail.com>
2025-08-17 18:58:20 +0900

    섹션 이미지 공간을 180x180으로 축소하고 드론 위치 조정: 스크롤 전 왼쪽 상단 위치, 150x150 이미지가 180x180 공간 정중앙에 정렬

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -14,11 +14,11 @@ const Home: React.FC = () => {
   // 드론 위치 애니메이션 - 각 섹션의 이미지 공간에 정중앙으로 이동
   const droneX = useTransform(scrollYProgress, 
     [0, 0.2, 0.4, 0.6, 0.8, 1], 
-    [0, -225, 225, -225, 225, 0]
+    [-75, -215, 215, -215, 215, -75]
   );
   const droneY = useTransform(scrollYProgress, 
     [0, 0.2, 0.4, 0.6, 0.8, 1], 
-    [1000, 125, 125, 125, 125, 125]
+    [1000, 115, 115, 115, 115, 115]
   );
   const droneScale = useTransform(scrollYProgress, [0, 0.5, 1], [1, 1, 1]);
   const droneOpacity = useTransform(scrollYProgress, [0, 0.1, 0.9, 1], [0, 1, 1, 0]);
